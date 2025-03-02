a6748b9b56ff14318735c111c453bc0af5a95f99
Yuki Hayashi <mrchildreny@gmail.com>
2025-03-02 12:24:41 +0900

    fix: remove unused parameter in global search params creation

--- packages/nextjs/src/tests/generated/safe-routes/types.ts
+++ packages/nextjs/src/tests/generated/safe-routes/types.ts
@@ -2,7 +2,7 @@ import { createSearchParams } from "@safe-routes/nextjs";
 /**
  * Global search parameters that will be applied to all routes
  */
-export const $SearchParams = createSearchParams((p) => ({
+export const $SearchParams = createSearchParams(() => ({
   // Add your global search parameters here
   // locale: p.enumOr(["en", "ja"] as const, "en"),
 })).passthrough();
