86ccc969ad0ecc902635a13ba75838683e84691d
Yuki Hayashi <mrchildreny@gmail.com>
2025-03-09 12:41:06 +0900

    fix: SearchParamsの型定義を改善し、より柔軟な型推論を実現

--- packages/next-typesafe-path/src/writer/createTypeDefinitionContent.ts
+++ packages/next-typesafe-path/src/writer/createTypeDefinitionContent.ts
@@ -23,9 +23,9 @@ export const createTypeDefinitionContent = ({
 declare module "@@@next-typesafe-path" {
   type IsSearchParams<T> = symbol extends keyof T ? false : true;
   type SearchParamsConfig = import("${options.configDir}/next-typesafe-path.config").SearchParams;
-  type SearchParams = IsSearchParams<SearchParamsConfig> extends true ? SearchParamsConfig : {};
+  type SearchParams = IsSearchParams<SearchParamsConfig> extends true ? SearchParamsConfig : { [key: string]: never };
   type ExportedQuery<T> = IsSearchParams<T> extends true
-    ? { [K in keyof T]: T[K] } & SearchParams
+    ? { [K in keyof T]: T[K] } | SearchParams
     : SearchParams;
   type RoutePath = ${routePaths}
 
